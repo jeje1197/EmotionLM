ed0f65c528604cc730d402978f485c49a7371ac0
jeje1197 <1jojorun@gmail.com>
2025-12-05 11:30:40 -0600

    Create directed graph data structure

--- emotion_lm/models/graph.py
+++ emotion_lm/models/graph.py
@@ -1,9 +1,68 @@
+"""
+Node: {id: str, data: any}
+Edge: {node_start, node_end}
+
+nodes: set
+adjacency list: dict[id:str, dict[id: str, data]]
+"""
+class Graph:
+    """
+    Implementation for a directed graph data structure.
+    """
+    def __init__(self):
+        self.nodes = set()
+        self.adjacency_list = dict()
+    
+    def node_count(self):
+        return len(self.nodes)
+    
+    def edge_count(self):
+        return len(self.adjacency_list)
+    
+    def get_node(self, id):
+        return self.nodes[id]
+    
+    def add_node(self, id):
+        self.nodes.add(id)
+
+    def add_edge(self, start_id, end_id, data = None):
+        if start_id not in self.nodes:
+            raise ValueError(f"Could not find node with id: '{start_id}'")
+        if end_id not in self.nodes:
+            raise ValueError(f"Could not find node with id: '{end_id}'")
+        if start_id not in self.adjacency_list:
+            self.adjacency_list[start_id] = dict()
+
+        self.adjacency_list[start_id][end_id] = data
+    
+    def remove_node(self, id):
+        self.nodes.remove(id)
+    
+    def remove_edge(self, start_id, end_id):
+        if start_id not in self.adjacency_list:
+            return
+        
+        del self.adjacency_list[start_id][end_id]
+        if len(self.adjacency_list[start_id]) == 0:
+            del self.adjacency_list[start_id]
+    
+    def stringify(self):
+        return f"""\
+Graph:
+Number of Nodes: {self.node_count()} Number of Edges: {self.edge_count()}
+Nodes: {self.nodes}
+Edges: {self.adjacency_list}
+"""
+    
+  
+
+
+
 
 
 class Node:
-    def __init__(self, id, memory):
+    def __init__(self, id):
         self.id = id
-        self.memory = memory
 
 class Edge:
     def __init__(self, from_node, to_node, type, metadata, weight):
